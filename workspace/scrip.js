a23f7ed64d2cb212f2587df69dbadf8210140a57
tom98159s <93498607+tom98159s@users.noreply.github.com>
2024-08-20 15:42:25 +0800

    version1

---
+++ workspace/scrip.js
@@ -0,0 +1,6 @@
+/*let food=prompt("how much is it?");
+let percent=prompt("how much percent is the tip");
+let money=food*(percent/100);
+let co="ur tips is ";
+let total=money+parseInt(food);
+alert(co+ money + " ,total is " + total)
