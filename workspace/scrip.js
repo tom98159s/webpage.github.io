2db34215ca93bd1ffd60bee349713ab697fe6750
tom98159s <93498607+tom98159s@users.noreply.github.com>
2024-08-26 02:55:15 +0800

    Add files via upload

--- workspace/scrip.js
+++ workspace/scrip.js
@@ -1,6 +1,15 @@
-/*let food=prompt("how much is it?");
-let percent=prompt("how much percent is the tip");
-let money=food*(percent/100);
-let co="ur tips is ";
-let total=money+parseInt(food);
-alert(co+ money + " ,total is " + total)
+// /*let food=prompt("how much is it?");
+// let percent=prompt("how much percent is the tip");
+// let money=food*(percent/100);
+// let co="ur tips is ";
+// let total=money+parseInt(food);
+// alert(co+ money + " ,total is " + total)
+
+
+
+window.onload=function()
+{
+var pcalart = "請切換成電腦版或用PC觀看唷，手機板尚未建置完成TT";
+
+alert(pcalart);
+};
